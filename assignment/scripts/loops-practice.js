ee3d3b538d00acd857f672318d51c290294a55e8
Kord Maurer <kord.r.maurer@gmail.com>
2023-04-03 13:27:41 -0500

    Completed required features and stretch goals.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -14,17 +14,36 @@ for (let i = 0; i < 4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
+// We change i < 4, as we're counting higher than the provided example but starting at the same value.
+
+for (let i=0; i < 6; i++) {
+  console.log(i);
+}
+
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+
+// We change i=0 to i=3, as that is where we're starting our count, and taking the i < 6 from the previous loop.
+
+for (let i=3; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+for (i=2; i < 12; i+=2) {
+  console.log(i);
+}
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
+for (i=5; i > -1; i--) {
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -33,6 +52,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
+for (star of stars) {
+console.log(star);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
@@ -40,8 +62,35 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+// Okay dude, you got me. This one BROKE MY BRAIN. It is so easy to conceptualize loops when pulling numerical values,
+// but my mind is absolutely melting trying to figure out how the hell to return index values of a string array.
+// Are you sure this wasn't supposed to be a stretch goal????
+
+i=0;
+
+while (i < stars.length){
+  console.log(stars[i]);
+  i++;
+}
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+i = 0;
+let max = 6;
+
+while(i < max){
+  console.log(i);
+  i++;
+}
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+i = 10;
+min = 4;
+
+while (i > min){
+  console.log(i);
+  i--;
+}
